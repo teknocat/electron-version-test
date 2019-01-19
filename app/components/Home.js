1ff4fc91c59060f6fb3be6ac4d33bf4109d40f8b
teknocat <teknocat@gmail.com>
2019-01-19 18:19:43 +0900

    Set application version string from a part of git hash value

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,11 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { remote } from 'electron';
 import routes from '../constants/routes';
 import styles from './Home.css';
+
+const { app } = remote;
 
 type Props = {};
 
@@ -14,6 +17,7 @@ export default class Home extends Component<Props> {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
+        <p>version {app.getVersion()}</p>
       </div>
     );
   }
